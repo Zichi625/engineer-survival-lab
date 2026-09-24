e1409751de59f05198873bd3c6da33742397b6fe
小琪 <service@hexschool.com>
2026-09-24 16:37:48 +0800

    chore: scaffold engineer-survival-lab static site

---
+++ config.example.js
@@ -0,0 +1,2 @@
+export const GAS_WEB_APP_URL = '';
+export const GAS_SHARED_SECRET = '';
---
+++ js/main.js
@@ -0,0 +1,2 @@
+document.getElementById('app').innerHTML =
+  '<p style="text-align:center; margin-top: 40vh; font-size: 20px;">🧪 工程師生存實驗室準備中……</p>';
